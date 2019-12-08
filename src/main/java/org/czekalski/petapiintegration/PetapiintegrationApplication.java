4b06786208b1ea36f3b89d988ced422b8fad21de
czarek <karakankaczyn@gmail.com>
2019-12-08 16:31:02 +0100

    prepared configuration

---
+++ src/main/java/org/czekalski/petapiintegration/PetapiintegrationApplication.java
@@ -0,0 +1,18 @@
+package org.czekalski.petapiintegration;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PetapiintegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetapiintegrationApplication.class, args);
+    }
+
+
+
+
+}
