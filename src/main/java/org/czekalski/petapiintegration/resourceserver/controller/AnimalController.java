7ea6cd70786201ed02cfbb85d3d04d349a971d38
czarek <karakankaczyn@gmail.com>
2020-02-07 23:22:29 +0100

    added HATEOAS

--- src/main/java/org/czekalski/petapiintegration/resourceserver/controller/AnimalController.java
+++ src/main/java/org/czekalski/petapiintegration/resourceserver/controller/AnimalController.java
@@ -1,27 +1,34 @@
 package org.czekalski.petapiintegration.resourceserver.controller;
 
+import org.czekalski.petapiintegration.config.AnimalRepresentationModelAssembler;
 import org.czekalski.petapiintegration.resourceserver.apiresource.v1.dto.AnimalResourcesListDto;
 import org.czekalski.petapiintegration.resourceserver.service.AnimalService;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class AnimalController {
     private final AnimalService animalService;
+    private static final AnimalRepresentationModelAssembler ANIMAL_REPRESENTATION_MODEL_ASSEMBLER = new AnimalRepresentationModelAssembler();
 
     public AnimalController(AnimalService animalService) {
         this.animalService = animalService;
     }
 
     @GetMapping("/dogs/{stateId}/{cityId}")
-    public AnimalResourcesListDto getDogsFromCity(
+    public EntityModel<AnimalResourcesListDto> getDogsFromCity(
             @PathVariable String stateId,
             @PathVariable String cityId,
             @RequestParam int size,
             @RequestParam("page") int page) {
-        return animalService.findDogsByCityIdAndStateId(stateId, cityId, size, page);
+
+        return ANIMAL_REPRESENTATION_MODEL_ASSEMBLER
+                .toModel(animalService
+                        .findDogsByCityIdAndStateId(stateId, cityId, size, page), stateId, cityId, size, page);
     }
 
 }
