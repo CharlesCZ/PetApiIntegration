84ba387beb179021aa223d40f7f13c7d78a768d9
czarek <cezaryczekalski@gmail.com>
2020-04-03 19:18:06 +0200

    test2

--- src/main/java/org/czekalski/petapiintegration/config/AnimalFactory/AfterAllPages.java
+++ src/main/java/org/czekalski/petapiintegration/config/AnimalFactory/AfterAllPages.java
@@ -23,7 +23,6 @@ final class AfterAllPages implements PaginationCase {
         int size = animalResourcesListDto.getPagination().getSize();
 
         return new EntityModel<>(animalResourcesListDto,
-              //ghfdgdf
                 linkTo(
                         methodOn(AnimalController.class)
                                 .getDogsFromCity(stateId, cityId, size, animalResourcesListDto.getPagination().getTotalPages()))
